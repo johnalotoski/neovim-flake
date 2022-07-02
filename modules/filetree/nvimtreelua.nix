06f9e6ba358766bda6cfaf53e4e29f24bc630f10
Michael Fellinger <michael.fellinger@iohk.io>
2022-07-02 19:28:59 +0200

    fix nvimtree options

--- modules/filetree/nvimtreelua.nix
+++ modules/filetree/nvimtreelua.nix
@@ -1,33 +1,38 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 with lib;
-with builtins;
-
-let
+with builtins; let
   cfg = config.vim.filetree.nvimTreeLua;
   pp = v: builtins.trace (builtins.toJSON v) v;
   indent = n: builtins.concatStringsSep "" (builtins.genList (a: " ") n);
   nl = "\n";
 
   # Totally naive translation of Nix expressions to Lua.
-  toLua = level: o:
-    let type = builtins.typeOf o;
-    in if type == "bool" then
-      lib.boolToString o
-    else if type == "string" then
-      ''"${o}"''
-    else if type == "list" then
-      "{" + (lib.concatStringsSep ", " (map toLua o)) + "}"
-    else if type == "int" then
-      toString o
-    else if type == "null" then
-      "nil"
-    else if type == "set" then ''
+  toLua = level: o: let
+    type = builtins.typeOf o;
+  in
+    if type == "bool"
+    then lib.boolToString o
+    else if type == "string"
+    then ''"${o}"''
+    else if type == "list"
+    then "{" + (lib.concatStringsSep ", " (map toLua o)) + "}"
+    else if type == "int"
+    then toString o
+    else if type == "null"
+    then "nil"
+    else if type == "set"
+    then ''
       {
       ${lib.concatStringsSep ",${nl}"
-      (lib.mapAttrsToList (k: v: "${indent level}${k} = ${toLua (level + 2) v}")
-        o)}
-      ${indent (level - 2)}}'' else
-      throw "Cannot translate unknown type '${type}' to Lua";
+        (lib.mapAttrsToList (k: v: "${indent level}${k} = ${toLua (level + 2) v}")
+          o)}
+      ${indent (level - 2)}}''
+    else throw "Cannot translate unknown type '${type}' to Lua";
 
   optionsToLua = toLua 2;
 in {
@@ -43,7 +48,7 @@ in {
     treeSide = mkOption {
       default = "left";
       description = "Side the tree will appear on left or right";
-      type = types.enum [ "left" "right" ];
+      type = types.enum ["left" "right"];
     };
 
     treeWidth = mkOption {
@@ -53,7 +58,7 @@ in {
     };
 
     hideFiles = mkOption {
-      default = [ ".git" "node_modules" ".cache" ];
+      default = [".git" "node_modules" ".cache"];
       description = "Files to hide in the file view by default";
       type = with types; listOf str;
     };
@@ -77,7 +82,7 @@ in {
     };
 
     ignoreFileTypes = mkOption {
-      default = [ "startify" ];
+      default = ["startify"];
       description = "Ignore file types";
       type = with types; listOf str;
     };
@@ -138,7 +143,7 @@ in {
 
     setup = mkOption {
       description = "all options that are configured in the setup hook";
-      default = { };
+      default = {};
       type = types.submodule {
         options = {
           disable_netrw = mkOption {
@@ -149,55 +154,41 @@ in {
 
           hijack_netrw = mkOption {
             default = true;
-            description =
-              "hijack netrw windows (overriden if disable_netrw is `true`)";
+            description = "hijack netrw windows (overriden if disable_netrw is `true`)";
             type = types.bool;
           };
 
           open_on_setup = mkOption {
             default = false;
-            description =
-              "will automatically open the tree when running setup if current buffer is a directory, is empty or is unnamed.";
+            description = "will automatically open the tree when running setup if current buffer is a directory, is empty or is unnamed.";
             type = types.bool;
           };
 
           ignore_ft_on_setup = mkOption {
-            description =
-              "list of filetypes that will make open_on_setup not open. You can use this option if you don't want the tree to open in some scenarios (eg using vim startify).";
+            description = "list of filetypes that will make open_on_setup not open. You can use this option if you don't want the tree to open in some scenarios (eg using vim startify).";
             type = types.listOf types.str;
-            default = [ ];
-          };
-
-          auto_close = mkOption {
-            description =
-              "force closing neovim when the tree is the last window in the view.";
-            type = types.bool;
-            default = false;
+            default = [];
           };
 
           open_on_tab = mkOption {
-            description =
-              "opens the tree automatically when switching tabpage or opening a new tabpage if the tree was previously open.";
+            description = "opens the tree automatically when switching tabpage or opening a new tabpage if the tree was previously open.";
             type = types.bool;
             default = false;
           };
 
-          update_to_buf_dir = mkOption {
-            description =
-              "hijacks new directory buffers when they are opened (`:e dir`).";
-            default = { };
+          hijack_directories = mkOption {
+            description = "hijacks new directory buffers when they are opened (`:e dir`).";
+            default = {};
             type = types.submodule {
               options = {
                 enable = mkOption {
-                  description =
-                    "enable the feature. Disable this option if you use vim-dirvish.";
+                  description = "enable the feature. Disable this option if you use vim-dirvish.";
                   type = types.bool;
                   default = true;
                 };
 
                 auto_open = mkOption {
-                  description =
-                    "opens the tree if the tree was previously closed.";
+                  description = "opens the tree if the tree was previously closed.";
                   type = types.bool;
                   default = true;
                 };
@@ -206,23 +197,20 @@ in {
           };
 
           hijack_cursor = mkOption {
-            description =
-              "keeps the cursor on the first letter of the filename when moving in the tree.";
+            description = "keeps the cursor on the first letter of the filename when moving in the tree.";
             type = types.bool;
             default = false;
           };
 
           update_cwd = mkOption {
-            description =
-              "changes the tree root directory on `DirChanged` and refreshes the tree.";
+            description = "changes the tree root directory on `DirChanged` and refreshes the tree.";
             type = types.bool;
             default = false;
           };
 
           update_focused_file = mkOption {
-            description =
-              "update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file";
-            default = { };
+            description = "update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file";
+            default = {};
             type = types.submodule {
               options = {
                 enable = mkOption {
@@ -241,10 +229,9 @@ in {
                 };
 
                 ignore_list = mkOption {
-                  description =
-                    "list of buffer names and filetypes that will not update the root dir of the tree if the file isn't found under the current root directory. Only relevant when |update_focused_file.update_cwd| is `true` and |update_focused_file.enable| is `true`.";
+                  description = "list of buffer names and filetypes that will not update the root dir of the tree if the file isn't found under the current root directory. Only relevant when |update_focused_file.update_cwd| is `true` and |update_focused_file.enable| is `true`.";
                   type = types.listOf types.str;
-                  default = [ ];
+                  default = [];
                 };
               };
             };
@@ -252,13 +239,11 @@ in {
 
           system_open = mkOption {
             description = "configuration options for the system open command";
-            default = { };
+            default = {};
             type = types.submodule {
               options = {
-
                 cmd = mkOption {
-                  description =
-                    "the command to run, leaving nil should work but useful if you want to override the default command with another one.";
+                  description = "the command to run, leaving nil should work but useful if you want to override the default command with another one.";
                   type = types.nullOr types.str;
                   default = null;
                 };
@@ -266,7 +251,7 @@ in {
                 args = mkOption {
                   description = "the command arguments as a list";
                   type = types.listOf types.str;
-                  default = [ ];
+                  default = [];
                 };
               };
             };
@@ -282,10 +267,9 @@ in {
               - `NvimTreeLspDiagnosticsInformation`
               - `NvimTreeLspDiagnosticsHint`
             '';
-            default = { };
+            default = {};
             type = types.submodule {
               options = {
-
                 enable = mkOption {
                   description = "enable/disable the feature";
                   type = types.bool;
@@ -320,7 +304,7 @@ in {
               milliseconds but a few seconds), it will not render anything until the git
               process returned the data.
             '';
-            default = { };
+            default = {};
 
             type = types.submodule {
               options = {
@@ -341,8 +325,7 @@ in {
                 };
 
                 timeout = mkOption {
-                  description =
-                    "kills the git process after some time (in ms) if it takes too long";
+                  description = "kills the git process after some time (in ms) if it takes too long";
                   type = types.ints.positive;
                   default = 400;
                 };
@@ -352,41 +335,31 @@ in {
 
           view = mkOption {
             description = "window / buffer setup";
-            default = { };
+            default = {};
             type = types.submodule {
               options = {
                 hide_root_folder = mkOption {
-                  description =
-                    "hide the path of the current working directory on top of the tree";
+                  description = "hide the path of the current working directory on top of the tree";
                   type = types.bool;
                   default = false;
                 };
 
                 width = mkOption {
-                  description =
-                    "width of the window, can be either a `%` string or a number representing columns. Only works with |view.side| `left` or `right`";
-                  type = types.oneOf [ types.str types.ints.positive ];
+                  description = "width of the window, can be either a `%` string or a number representing columns. Only works with |view.side| `left` or `right`";
+                  type = types.oneOf [types.str types.ints.positive];
                   default = 30;
                 };
 
                 height = mkOption {
-                  description =
-                    "height of the window, can be either a `%` string or a number representing rows. Only works with |view.side| `top` or `bottom`";
-                  type = types.oneOf [ types.str types.ints.positive ];
+                  description = "height of the window, can be either a `%` string or a number representing rows. Only works with |view.side| `top` or `bottom`";
+                  type = types.oneOf [types.str types.ints.positive];
                   default = 30;
                 };
 
                 side = mkOption {
-                  description =
-                    "side of the tree, can be one of 'left' | 'right' | 'bottom' | 'top' Note that bottom/top are not working correctly yet.";
-                  type = types.enum [ "left" "right" "bottom" "top" ];
+                  description = "side of the tree, can be one of 'left' | 'right' | 'bottom' | 'top' Note that bottom/top are not working correctly yet.";
+                  type = types.enum ["left" "right" "bottom" "top"];
                   default = "left";
-                };
-
-                auto_resize = mkOption {
-                  description = "auto resize the tree after opening a file";
-                  type = types.bool;
-                  default = false;
                 };
 
                 number = mkOption {
@@ -396,29 +369,26 @@ in {
                 };
 
                 relativenumber = mkOption {
-                  description =
-                    "show the line number relative to the line with the cursor in front of each line. If the option `view.number` is also `true`, the number on the cursor line will be the line number instead of `0`.";
+                  description = "show the line number relative to the line with the cursor in front of each line. If the option `view.number` is also `true`, the number on the cursor line will be the line number instead of `0`.";
                   type = types.bool;
                   default = false;
                 };
 
                 mappings = mkOption {
                   description = "configuration options for keymaps";
-                  default = { };
+                  default = {};
                   type = types.submodule {
                     options = {
                       custom_only = mkOption {
-                        description =
-                          "will use only the provided user mappings and not the default otherwise, extends the default mappings with the provided user mappings";
+                        description = "will use only the provided user mappings and not the default otherwise, extends the default mappings with the provided user mappings";
                         type = types.bool;
                         default = false;
                       };
 
                       list = mkOption {
-                        description =
-                          "a list of keymaps that will extend or override the default keymaps";
+                        description = "a list of keymaps that will extend or override the default keymaps";
                         type = types.attrsOf types.str;
-                        default = { };
+                        default = {};
                       };
                     };
                   };
@@ -429,12 +399,11 @@ in {
 
           filters = mkOption {
             description = "filtering options";
-            default = { };
+            default = {};
             type = types.submodule {
               options = {
                 dotfiles = mkOption {
-                  description =
-                    "do not show `dotfiles` (files starting with a `.`)";
+                  description = "do not show `dotfiles` (files starting with a `.`)";
                   type = types.bool;
                   default = false;
                 };
@@ -442,7 +411,7 @@ in {
                 custom = mkOption {
                   description = "custom list of string that will not be shown.";
                   type = types.listOf types.str;
-                  default = [ ];
+                  default = [];
                 };
               };
             };
@@ -450,12 +419,11 @@ in {
 
           trash = mkOption {
             description = "configuration options for trashing";
-            default = { };
+            default = {};
             type = types.submodule {
               options = {
                 cmd = mkOption {
-                  description =
-                    "the command used to trash items (must be installed on your system)";
+                  description = "the command used to trash items (must be installed on your system)";
                   type = types.path;
                   default = "${pkgs.trash-cli}/bin/trash";
                 };
@@ -473,11 +441,19 @@ in {
     };
   };
 
-  config = mkIf cfg.enable (let mkVimBool = val: if val then 1 else 0;
+  config = mkIf cfg.enable (let
+    mkVimBool = val:
+      if val
+      then 1
+      else 0;
   in {
     vim.startPlugins = with pkgs.neovimPlugins; [
       nvim-tree-lua
-      (if cfg.devIcons then nvim-web-devicons else null)
+      (
+        if cfg.devIcons
+        then nvim-web-devicons
+        else null
+      )
     ];
 
     vim.nnoremap = {
@@ -500,4 +476,3 @@ in {
     '';
   });
 }
-
