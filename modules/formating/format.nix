59da39c0acbec9baf3bbff4284fb4c3883788c38
Michael Fellinger <michael.fellinger@iohk.io>
2022-08-20 05:16:21 +0200

    format gleam and erlang

--- modules/formating/format.nix
+++ modules/formating/format.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   config = {
-    vim.startPlugins = with pkgs.neovimPlugins; [ formatter-nvim ];
+    vim.startPlugins = with pkgs.neovimPlugins; [formatter-nvim];
 
     vim.configRC = ''
       augroup FormatAutogroup
@@ -33,48 +33,51 @@
           ruby = treefmt,
           rust = treefmt,
           vim = treefmt,
+          erlang = treefmt,
+          gleam = treefmt,
         }
       })
     '';
 
-    /* rust = {{ cmd = {"rustfmt --edition 2018"} }},
-              cue = {{ cmd = {"${pkgs.cue}/bin/cue fmt"} }},
-              mint = {{ cmd = {"mint format"} }},
-              nix = {{
-                cmd = {
-                  "${pkgs.statix}/bin/statix fix",
-                  "${pkgs.treefmt}/bin/treefmt",
-                }
-              }},
-              rego = {{ cmd = {"${pkgs.treefmt}/bin/treefmt"} }},
-              crystal = {{ cmd = {"crystal tool format"} }},
-              vim = {{
-                cmd = {"luafmt -w replace"},
-                start_pattern = "^lua << EOF$",
-                end_pattern = "^EOF$"
-              }},
-              vimwiki = {{
-                cmd = {"prettier -w --parser babel"},
-                start_pattern = "^{{{javascript$",
-                end_pattern = "^}}}$"
-              }},
-              lua = {{
-                cmd = {
-                  function(file)
-                    return string.format("luafmt -l %s -w replace %s", vim.bo.textwidth, file)
-                  end
-                }
-              }},
-              go = {{
-                cmd = {"gofmt -w", "goimports -w"},
-                tempfile_postfix = ".tmp",
-              }},
-              javascript = {{
-                cmd = {"prettier --no-semi -w"},
-                tempfile_postfix = ".tmp",
-              }},
-            }
-          '';
-    */
+    /*
+     rust = {{ cmd = {"rustfmt --edition 2018"} }},
+            cue = {{ cmd = {"${pkgs.cue}/bin/cue fmt"} }},
+            mint = {{ cmd = {"mint format"} }},
+            nix = {{
+              cmd = {
+                "${pkgs.statix}/bin/statix fix",
+                "${pkgs.treefmt}/bin/treefmt",
+              }
+            }},
+            rego = {{ cmd = {"${pkgs.treefmt}/bin/treefmt"} }},
+            crystal = {{ cmd = {"crystal tool format"} }},
+            vim = {{
+              cmd = {"luafmt -w replace"},
+              start_pattern = "^lua << EOF$",
+              end_pattern = "^EOF$"
+            }},
+            vimwiki = {{
+              cmd = {"prettier -w --parser babel"},
+              start_pattern = "^{{{javascript$",
+              end_pattern = "^}}}$"
+            }},
+            lua = {{
+              cmd = {
+                function(file)
+                  return string.format("luafmt -l %s -w replace %s", vim.bo.textwidth, file)
+                end
+              }
+            }},
+            go = {{
+              cmd = {"gofmt -w", "goimports -w"},
+              tempfile_postfix = ".tmp",
+            }},
+            javascript = {{
+              cmd = {"prettier --no-semi -w"},
+              tempfile_postfix = ".tmp",
+            }},
+          }
+        '';
+     */
   };
 }
