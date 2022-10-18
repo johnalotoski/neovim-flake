49817cd71ce80ef57af54544dc0cce62094cd9c8
Michael Fellinger <michael.fellinger@iohk.io>
2022-10-18 23:52:03 +0200

    earlier completion start

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -282,9 +282,9 @@ in {
         },
         sources = cmp.config.sources {
           { name = 'path' },
-          { name = 'nvim_lsp', keyword_length = 3 },
-          { name = 'luasnip', keyword_length = 3 },
-          { name = 'buffer', keyword_length = 3 },
+          { name = 'nvim_lsp', keyword_length = 1 },
+          { name = 'luasnip', keyword_length = 1 },
+          { name = 'buffer', keyword_length = 1 },
         },
         window = {
           documentation = cmp.config.window.bordered()
