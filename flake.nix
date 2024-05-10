207e417c3c79e6cd038a1ef027b858a0140075a2
John Lotoski <john.lotoski@iohk.io>
2024-05-10 12:16:00 -0500

    imp: Adds hls lsp, vim-ormolu for auto-format and does alpha-sort cleanup

--- flake.nix
+++ flake.nix
@@ -14,236 +14,240 @@
     nil.url = "github:oxalica/nil";
 
     # Vim plugins
-    elixir-nvim = {
-      url = "github:mhanberg/elixir.nvim";
-      flake = false;
-    };
-    gleam-vim = {
-      url = "github:gleam-lang/gleam.vim";
-      flake = false;
-    };
-    zig-vim = {
-      url = "github:ziglang/zig.vim";
-      flake = false;
-    };
-    vim-nickel = {
-      url = "github:nickel-lang/vim-nickel";
-      flake = false;
-    };
-    gruvbox = {
-      url = "github:morhetz/gruvbox";
-      flake = false;
-    };
-    nord-vim = {
-      url = "github:arcticicestudio/nord-vim";
-      flake = false;
-    };
-    vim-startify = {
-      url = "github:mhinz/vim-startify";
-      flake = false;
-    };
-    lightline-vim = {
-      url = "github:itchyny/lightline.vim";
-      flake = false;
-    };
-    nvim-lspconfig = {
-      url = "github:neovim/nvim-lspconfig";
-      flake = false;
-    };
-    vim-nix = {
-      url = "github:LnL7/vim-nix";
-      flake = false;
-    };
-    nvim-dap = {
-      url = "github:mfussenegger/nvim-dap";
-      flake = false;
-    };
-    nvim-telescope = {
-      url = "github:nvim-telescope/telescope.nvim";
-      flake = false;
-    };
-    telescope-dap = {
-      url = "github:nvim-telescope/telescope-dap.nvim";
-      flake = false;
-    };
-    popup-nvim = {
-      url = "github:nvim-lua/popup.nvim";
-      flake = false;
-    };
-    plenary-nvim = {
-      url = "github:nvim-lua/plenary.nvim";
-      flake = false;
-    };
-    nvim-web-devicons = {
-      url = "github:kyazdani42/nvim-web-devicons";
-      flake = false;
-    };
-    nvim-tree-lua = {
-      url = "github:kyazdani42/nvim-tree.lua";
-      flake = false;
-    };
-    vimagit = {
-      url = "github:jreybert/vimagit";
-      flake = false;
-    };
-    nvim-lightbulb = {
-      url = "github:kosayoda/nvim-lightbulb";
-      flake = false;
-    };
-    nvim-treesitter = {
-      url = "github:nvim-treesitter/nvim-treesitter";
-      flake = false;
-    };
-    nvim-treesitter-context = {
-      url = "github:romgrk/nvim-treesitter-context";
-      flake = false;
-    };
     barbar-nvim = {
       url = "github:romgrk/barbar.nvim";
-      flake = false;
-    };
-    editorconfig-vim = {
-      url = "github:editorconfig/editorconfig-vim";
-      flake = false;
-    };
-    indent-blankline-nvim = {
-      url = "github:lukas-reineke/indent-blankline.nvim";
-      flake = false;
-    };
-    nvim-blame-line = {
-      url = "github:tveskag/nvim-blame-line";
-      flake = false;
-    };
-    nvim-dap-virtual-text = {
-      url = "github:theHamsta/nvim-dap-virtual-text";
-      flake = false;
-    };
-    vim-cursorword = {
-      url = "github:itchyny/vim-cursorword";
-      flake = false;
-    };
-    vim-dadbod = {
-      url = "github:tpope/vim-dadbod";
-      flake = false;
-    };
-    vim-dadbod-ui = {
-      url = "github:kristijanhusak/vim-dadbod-ui";
-      flake = false;
-    };
-    vim-hexokinase = {
-      url = "github:RRethy/vim-hexokinase";
-      flake = false;
-    };
-    which-key-nvim = {
-      url = "github:folke/which-key.nvim";
-      flake = false;
-    };
-    vim-test = {
-      url = "github:vim-test/vim-test";
-      flake = false;
-    };
-    vim-floaterm = {
-      url = "github:voldikss/vim-floaterm";
-      flake = false;
-    };
-    vim-crystal = {
-      url = "github:vim-crystal/vim-crystal";
-      flake = false;
-    };
-    nvim-jqx = {
-      url = "github:gennaro-tedesco/nvim-jqx";
-      flake = false;
-    };
-    formatter-nvim = {
-      url = "github:mhartington/formatter.nvim";
-      flake = false;
-    };
-    vim-cue = {
-      url = "github:jjo/vim-cue";
-      flake = false;
-    };
-    vim-mint = {
-      url = "github:IrenejMarc/vim-mint";
-      flake = false;
-    };
-    vim-surround = {
-      url = "github:tpope/vim-surround";
-      flake = false;
-    };
-    wilder-nvim = {
-      url = "github:gelguy/wilder.nvim";
-      flake = false;
-    };
-    vim-abolish = {
-      url = "github:tpope/vim-abolish";
-      flake = false;
-    };
-    vim-go = {
-      url = "github:fatih/vim-go";
-      flake = false;
-    };
-    nvim-idris2 = {
-      url = "github:ShinKage/nvim-idris2";
-      flake = false;
-    };
-    cmp-nvim-lsp = {
-      url = "github:hrsh7th/cmp-nvim-lsp";
       flake = false;
     };
     cmp-buffer = {
       url = "github:hrsh7th/cmp-buffer";
       flake = false;
     };
-    cmp-path = {
-      url = "github:hrsh7th/cmp-path";
-      flake = false;
-    };
     cmp-cmdline = {
       url = "github:hrsh7th/cmp-cmdline";
-      flake = false;
-    };
-    nvim-cmp = {
-      url = "github:hrsh7th/nvim-cmp";
-      flake = false;
-    };
-    LuaSnip = {
-      url = "github:L3MON4D3/LuaSnip";
       flake = false;
     };
     cmp_luasnip = {
       url = "github:saadparwaiz1/cmp_luasnip";
       flake = false;
     };
-    lsp_signature = {
-      url = "github:ray-x/lsp_signature.nvim";
+    cmp-nvim-lsp = {
+      url = "github:hrsh7th/cmp-nvim-lsp";
       flake = false;
     };
-    splice = {
-      url = "github:sjl/splice.vim";
-      flake = false;
-    };
-    vim-slim = {
-      url = "github:slim-template/vim-slim";
-      flake = false;
-    };
-    vim-just = {
-      url = "github:NoahTheDuke/vim-just";
-      flake = false;
-    };
-    no-neck-pain = {
-      url = "github:shortcuts/no-neck-pain.nvim";
+    cmp-path = {
+      url = "github:hrsh7th/cmp-path";
       flake = false;
     };
     copilot-vim = {
       url = "github:github/copilot.vim";
       flake = false;
     };
-    nvim-nu = {
-      url = "github:LhKipp/nvim-nu";
+    editorconfig-vim = {
+      url = "github:editorconfig/editorconfig-vim";
+      flake = false;
+    };
+    elixir-nvim = {
+      url = "github:mhanberg/elixir.nvim";
+      flake = false;
+    };
+    formatter-nvim = {
+      url = "github:mhartington/formatter.nvim";
+      flake = false;
+    };
+    gleam-vim = {
+      url = "github:gleam-lang/gleam.vim";
+      flake = false;
+    };
+    gruvbox = {
+      url = "github:morhetz/gruvbox";
+      flake = false;
+    };
+    indent-blankline-nvim = {
+      url = "github:lukas-reineke/indent-blankline.nvim";
+      flake = false;
+    };
+    lightline-vim = {
+      url = "github:itchyny/lightline.vim";
+      flake = false;
+    };
+    lsp_signature = {
+      url = "github:ray-x/lsp_signature.nvim";
+      flake = false;
+    };
+    LuaSnip = {
+      url = "github:L3MON4D3/LuaSnip";
+      flake = false;
+    };
+    no-neck-pain = {
+      url = "github:shortcuts/no-neck-pain.nvim";
+      flake = false;
+    };
+    nord-vim = {
+      url = "github:arcticicestudio/nord-vim";
       flake = false;
     };
     null-ls-nvim = {
       url = "github:jose-elias-alvarez/null-ls.nvim";
+      flake = false;
+    };
+    nvim-blame-line = {
+      url = "github:tveskag/nvim-blame-line";
+      flake = false;
+    };
+    nvim-cmp = {
+      url = "github:hrsh7th/nvim-cmp";
+      flake = false;
+    };
+    nvim-dap = {
+      url = "github:mfussenegger/nvim-dap";
+      flake = false;
+    };
+    nvim-dap-virtual-text = {
+      url = "github:theHamsta/nvim-dap-virtual-text";
+      flake = false;
+    };
+    nvim-idris2 = {
+      url = "github:ShinKage/nvim-idris2";
+      flake = false;
+    };
+    nvim-jqx = {
+      url = "github:gennaro-tedesco/nvim-jqx";
+      flake = false;
+    };
+    nvim-lightbulb = {
+      url = "github:kosayoda/nvim-lightbulb";
+      flake = false;
+    };
+    nvim-lspconfig = {
+      url = "github:neovim/nvim-lspconfig";
+      flake = false;
+    };
+    nvim-nu = {
+      url = "github:LhKipp/nvim-nu";
+      flake = false;
+    };
+    nvim-telescope = {
+      url = "github:nvim-telescope/telescope.nvim";
+      flake = false;
+    };
+    nvim-tree-lua = {
+      url = "github:kyazdani42/nvim-tree.lua";
+      flake = false;
+    };
+    nvim-treesitter-context = {
+      url = "github:romgrk/nvim-treesitter-context";
+      flake = false;
+    };
+    nvim-treesitter = {
+      url = "github:nvim-treesitter/nvim-treesitter";
+      flake = false;
+    };
+    nvim-web-devicons = {
+      url = "github:kyazdani42/nvim-web-devicons";
+      flake = false;
+    };
+    plenary-nvim = {
+      url = "github:nvim-lua/plenary.nvim";
+      flake = false;
+    };
+    popup-nvim = {
+      url = "github:nvim-lua/popup.nvim";
+      flake = false;
+    };
+    splice = {
+      url = "github:sjl/splice.vim";
+      flake = false;
+    };
+    telescope-dap = {
+      url = "github:nvim-telescope/telescope-dap.nvim";
+      flake = false;
+    };
+    vim-abolish = {
+      url = "github:tpope/vim-abolish";
+      flake = false;
+    };
+    vimagit = {
+      url = "github:jreybert/vimagit";
+      flake = false;
+    };
+    vim-crystal = {
+      url = "github:vim-crystal/vim-crystal";
+      flake = false;
+    };
+    vim-cue = {
+      url = "github:jjo/vim-cue";
+      flake = false;
+    };
+    vim-cursorword = {
+      url = "github:itchyny/vim-cursorword";
+      flake = false;
+    };
+    vim-dadbod-ui = {
+      url = "github:kristijanhusak/vim-dadbod-ui";
+      flake = false;
+    };
+    vim-dadbod = {
+      url = "github:tpope/vim-dadbod";
+      flake = false;
+    };
+    vim-floaterm = {
+      url = "github:voldikss/vim-floaterm";
+      flake = false;
+    };
+    vim-go = {
+      url = "github:fatih/vim-go";
+      flake = false;
+    };
+    vim-hexokinase = {
+      url = "github:RRethy/vim-hexokinase";
+      flake = false;
+    };
+    vim-just = {
+      url = "github:NoahTheDuke/vim-just";
+      flake = false;
+    };
+    vim-mint = {
+      url = "github:IrenejMarc/vim-mint";
+      flake = false;
+    };
+    vim-nickel = {
+      url = "github:nickel-lang/vim-nickel";
+      flake = false;
+    };
+    vim-nix = {
+      url = "github:LnL7/vim-nix";
+      flake = false;
+    };
+    vim-ormolu = {
+      url = "github:sdiehl/vim-ormolu";
+      flake = false;
+    };
+    vim-slim = {
+      url = "github:slim-template/vim-slim";
+      flake = false;
+    };
+    vim-startify = {
+      url = "github:mhinz/vim-startify";
+      flake = false;
+    };
+    vim-surround = {
+      url = "github:tpope/vim-surround";
+      flake = false;
+    };
+    vim-test = {
+      url = "github:vim-test/vim-test";
+      flake = false;
+    };
+    which-key-nvim = {
+      url = "github:folke/which-key.nvim";
+      flake = false;
+    };
+    wilder-nvim = {
+      url = "github:gelguy/wilder.nvim";
+      flake = false;
+    };
+    zig-vim = {
+      url = "github:ziglang/zig.vim";
       flake = false;
     };
   };
@@ -268,24 +272,11 @@
         overlays = [
           pluginOverlay
           (final: prev: {
-            statix = inputs.statix.defaultPackage.${system};
             inherit (inputs.nil.packages.${system}) nil;
-            tsserver = prev.nodePackages.typescript-language-server;
-            vimls = prev.nodePackages.vim-language-server;
-            yamlls = prev.nodePackages.yaml-language-server;
-            dockerls = prev.nodePackages.dockerfile-language-server-nodejs;
-            cssls = prev.nodePackages.vscode-css-languageserver-bin;
-            htmlls = prev.nodePackages.vscode-html-languageserver-bin;
-            jsonls = prev.nodePackages.vscode-json-languageserver-bin;
-            bashls = prev.nodePackages.bash-language-server;
 
-            neovim-nightly =
-              neovim.defaultPackage.${system}.overrideAttrs
-              (_: {
-                patches = [
-                  "${inputs.nixpkgs}/pkgs/applications/editors/neovim/system_rplugin_manifest.patch"
-                ];
-              });
+            bashls = prev.nodePackages.bash-language-server;
+            cssls = prev.nodePackages.vscode-css-languageserver-bin;
+            dockerls = prev.nodePackages.dockerfile-language-server-nodejs;
 
             efm-langserver = prev.buildGoModule rec {
               pname = "efm-langserver";
@@ -299,6 +290,23 @@
               };
             };
 
+            elixirls = prev.fetchzip {
+              url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.11.0/elixir-ls.zip";
+              hash = "sha256-Q1c+HMK9mhIX4bK9OddfckiR3gpxu9bITI5ED8FCHmI=";
+              stripRoot = false;
+            };
+
+            htmlls = prev.nodePackages.vscode-html-languageserver-bin;
+            jsonls = prev.nodePackages.vscode-json-languageserver-bin;
+
+            neovim-nightly =
+              neovim.defaultPackage.${system}.overrideAttrs
+              (_: {
+                patches = [
+                  "${inputs.nixpkgs}/pkgs/applications/editors/neovim/system_rplugin_manifest.patch"
+                ];
+              });
+
             regols = prev.buildGoModule rec {
               pname = "regols";
               version = "0.1.0";
@@ -311,11 +319,10 @@
               };
             };
 
-            elixirls = prev.fetchzip {
-              url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.11.0/elixir-ls.zip";
-              hash = "sha256-Q1c+HMK9mhIX4bK9OddfckiR3gpxu9bITI5ED8FCHmI=";
-              stripRoot = false;
-            };
+            statix = inputs.statix.defaultPackage.${system};
+            tsserver = prev.nodePackages.typescript-language-server;
+            vimls = prev.nodePackages.vim-language-server;
+            yamlls = prev.nodePackages.yaml-language-server;
           })
         ];
       };
@@ -358,26 +365,26 @@
           virtualedit = "all";
 
           editor = {
-            surround = true;
-            colourPreview = true;
-            whichKey = true;
-            floaterm = true;
-            wilder = true;
             abolish = true;
+            colourPreview = true;
+            floaterm = true;
+            surround = true;
+            whichKey = true;
+            wilder = true;
           };
 
           filetree.nvimTreeLua = {enable = true;};
           formatting.editorConfig.enable = true;
           fuzzyfind.telescope.enable = true;
+          lineNumberMode = "number";
+          showTrailingWhitespace = true;
           statusline.lightline.enable = true;
           tabbar.barbar.enable = true;
           test.enable = true;
           theme.gruvbox.enable = true;
+          trimTrailingWhitespace = true;
           viAlias = false;
           vimAlias = false;
-          lineNumberMode = "number";
-          showTrailingWhitespace = true;
-          trimTrailingWhitespace = true;
 
           git = {
             enable = true;
